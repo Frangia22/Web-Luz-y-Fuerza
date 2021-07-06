3bb63ef4260b719fd8bb090722fde1eb26653c17
Frangia22 <giacherofranc22@gmail.com>
2021-07-06 11:58:28 -0300

    mobile casi listo

--- static/js/effects.js
+++ static/js/effects.js
@@ -1,2 +1,5 @@
+menubtn.addEventListener('click', () => {
+	nav.classList.toggle('desplegar');
+});
 const turnoDadoPor = document.querySelector('#id_turnoDadoPor');
 turnoDadoPor.classList.add('form-control');
