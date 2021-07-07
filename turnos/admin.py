169b1000738b64fd8b7d6fe4edb91fb8e3734c48
Frangia22 <giacherofranc22@gmail.com>
2021-07-07 20:09:24 -0300

    Delete apellido de admin

--- turnos/admin.py
+++ turnos/admin.py
@@ -4,6 +4,6 @@ from turnos.models import  Turno
 @admin.register(Turno)
 class TurnoAdmin(admin.ModelAdmin):
     
-    list_display = ('nombre', 'apellido', 'tipoCancha', 'fecha', 'celular', 'montoRecaudado', 'turnoDadoPor')
+    list_display = ('nombre', 'tipoCancha', 'fecha', 'celular', 'montoRecaudado', 'turnoDadoPor')
 
 
