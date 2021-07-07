0ec0b85228b91537ba3bfdb47f7334a04e326507
Frangia22 <giacherofranc22@gmail.com>
2021-07-07 11:00:14 -0300

    Cambios en la view

--- turnos/views.py
+++ turnos/views.py
@@ -12,12 +12,13 @@ class nuevoTurno(LoginRequiredMixin, CreateView):
     model = Turno
     form_class = turnoForm
     template_name = "turnos/nuevoTurno.html"
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('detalleTurno')
 class detalleTurno(LoginRequiredMixin, ListView):
     model = Turno
     template_name = "turnos/detalleTurno.html"
     context_object_name = 'turnos'
     paginate_by = 7
+    ordering = ['-fecha']
 
 class eliminarTurno(LoginRequiredMixin, DeleteView):
     model = Turno
@@ -28,5 +29,5 @@ class editarTurno(LoginRequiredMixin, UpdateView):
     model = Turno
     form_class = turnoForm
     template_name = "turnos/editarTurno.html"
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('detalleTurno')
 
