d007fabb1cd35dd52ba2ee4d4a3ede1db62e9a45
Frangia22 <giacherofranc22@gmail.com>
2021-07-08 11:55:59 -0300

    Cambio en model campo dinero

--- turnos/models.py
+++ turnos/models.py
@@ -13,7 +13,7 @@ class Turno(models.Model):
     fecha = models.DateTimeField(verbose_name='Fecha y hora')
     celular = models.CharField("Celular", max_length = 11, null=True, default='', blank=True)
     asistio = models.CharField(max_length=9, choices= asistencia, default='Si')
-    montoRecaudado = models.IntegerField(default='', null=True)
+    montoRecaudado = models.IntegerField(default='', null=True, blank=True)
     turnoDadoPor = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="TurnoDadoPor")
 
 class Meta:
