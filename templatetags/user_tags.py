dbd82134c3642f246d45228740087d1979eac2a6
Frangia22 <giacherofranc22@gmail.com>
2021-07-08 12:51:34 -0300

    Ultimando detalles

--- templatetags/user_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.contrib.auth.models import Group
-register = template.Library()
-@register.filter
-def absoluto(value):
-    return abs(value)
-@register.filter(name='grupo')
-def grupo(usuario, grupo):
-    return usuario.groups.filter(name=grupo).exists()
