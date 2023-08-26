288e528fce9a653986a2aceeb8636e55135a293e
frangia22 <giacherofranc22@gmail.com>
2023-08-26 14:54:26 -0300

    Fix: permission users

---
+++ turnos/templatetags/has_group.py
@@ -0,0 +1,6 @@
+from django import template
+register = template.Library()
+
+@register.filter(name= 'has_group')
+def has_group(user, grupo):
+    return user.groups.filter(name__exact = grupo).exists()
